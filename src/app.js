158825d31c99b66dc84302b242cf4a0d465861a3
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-02-28 21:47:34 +0100

    Working emotion Removed `lib` folder from git

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 export const App = () => {
   return (
-    <div>
-      <p>Siema</p>
-    </div>
+    <>
+      <h1>Licznik</h1>
+      <p>Robię to, żebyś ty nie musiał...</p>
+      <hr />
+      <div></div>
+    </>
   );
 };
 
