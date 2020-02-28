53a8a65d365e98668323da38fcb9e03a9f0e898d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-02-28 22:34:04 +0100

    Some fancy styling 🎨

--- src/app.js
+++ src/app.js
@@ -7,15 +7,52 @@ import Change from "./components/Counter/Change.js";
 import { Provider } from "react-redux";
 import store from "./store.js";
 
+const serifFont = '"Playfair Display", "Times New Roman", Times, serif';
+
 export const App = () => {
   return (
     <>
-      <h1>Licznik</h1>
-      <p>Robię to, żebyś ty nie musiał...</p>
-      <hr />
-      <div></div>
-      <Display />
-      <Change />
+      <header
+        css={css`
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          align-items: center;
+        `}>
+        <h1
+          css={css`
+            font-family: ${serifFont};
+            font-weight: bold;
+            margin-bottom: 10px;
+          `}>
+          Licznik
+        </h1>
+        <p
+          css={css`
+            font-family: ${serifFont};
+            font-size: 1.1em;
+            margin-top: 0;
+          `}>
+          "Robię to, żebyś ty nie musiał..."
+        </p>
+      </header>
+      <hr
+        css={css`
+          height: 15px;
+          border: 0;
+          box-shadow: 0 10px 10px -10px #8c8b8b inset;
+        `}
+      />
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-content: center;
+          line-height: 1.45;
+        `}>
+        <Display />
+        <Change />
+      </div>
     </>
   );
 };
--- src/components/Counter/Display.js
+++ src/components/Counter/Display.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
+import { css } from "@emotion/core";
 
 const Display = ({ count }) => {
-  return <div>Stan licznika: {count}</div>;
+  return (
+    <div
+      css={css`
+        margin-right: 15px;
+      `}>
+      Stan licznika:{" "}
+      <span
+        css={css`
+          font-size: 1.05em;
+          font-weight: 700;
+        `}>
+        {count}
+      </span>
+    </div>
+  );
 };
 
 const mapStateToProps = state => ({
