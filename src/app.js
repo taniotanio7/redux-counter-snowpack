cd4338c3e3e3f53b9bd225f5b4c152f32176bf59
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2020-02-28 21:08:23 +0100

    🎉 Initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const App = () => {
+  return (
+    <div>
+      <p>Siema</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
